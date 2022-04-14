b2eb14b8b66bbb80af1bac172eca540df425310e
ashrafbd1496 <asraf1496@gmail.com>
2022-04-14 11:42:57 +0600

    Described threee types of loop

--- loop.js
+++ loop.js
@@ -1,3 +1,25 @@
+/**
 for (let i = 1; i <=  1000; i++) {
+	//every code written inside this block will execute multiple times.
 	console.log(i, 'Shaker Hossain');
 }
+*/
+
+
+/**
+ * Thers has three types of loop available in js
+ * 1. For (When we know the range)
+ * 2. While (when we don't know the range , know only condition)
+ * 3. Do while(Code will mustly execute first time before check codition. )
+ */
+/**
+while(true){
+	let num = Math.ceil(Math.random() * 100); //it will generate random number
+	console.log(num);
+	if (num == 99) break;
+}
+*/
+
+do{
+	console.log('Print this line at least one time');
+}while(false)
